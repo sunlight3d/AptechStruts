1b26ff8a9636d39b1e955c0e8af40a76a8b1f9e0
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-03-29 11:27:11 +0700

    update

---
+++ bai01/src/java/model/Message.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+public class Message {
+    private String message = "Hello world";
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+}
---
+++ bai01/web/message.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : message
+    Created on : Mar 29, 2019, 11:09:47 AM
+    Author     : Nguyen Duc Hoang
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1></h1>
+    </body>
+</html>
